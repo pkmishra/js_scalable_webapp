031fc2cec1ba9ded0c8e77ea3d5e3ccbad8f0076
Pradeep Kumar Mishra <pkmishra@gmail.com>
2012-05-09 11:29:13 -0400

    Updated Jquery references in test

--- test/test.ml.base.js
+++ test/test.ml.base.js
@@ -16,28 +16,6 @@
  */
 
 /*jslint vars:true */
-<<<<<<< HEAD
-/*global define, test, stop, start, ok, equal, notEqual, strictEqual, notStrictEqual, deepEqual, notDeepEqual, raises */
-define([
-	"lb/lb.base"
-], function (
-	base
-) {
-	"use strict";
-	
-	
-	function run() {
-		test("jQuery", 1, function () {
-			var jQuery = base.jQuery;
-			
-			ok(jQuery, "Test if jQuery is defined");
-		});
-	}
-
-	return {
-		run: run
-	};
-=======
 /*global define, test, ok, equal */
 define([
 	"lb/lb.base"
@@ -64,5 +42,4 @@ define([
 	};
 	
 	return tests;
->>>>>>> branch 'master' of git@github.com:manakor/manakorJS
 });
